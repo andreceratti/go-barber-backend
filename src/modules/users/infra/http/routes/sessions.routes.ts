52d671fa118226fc969683aad2bfca0a80d291be
Andre Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2020-08-13 20:58:54 -0300

    +Adicionando controllers que foram esquecidos +Atualizado sessions para usar controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,23 +1,10 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsControler from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsControler = new SessionsControler();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.send({ user, token });
-});
+sessionsRouter.post('/', sessionsControler.create);
 
 export default sessionsRouter;
