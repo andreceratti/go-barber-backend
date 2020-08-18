dc44cc162cbf6b1f87e6dd87db14dee23c8959f4
Andre Ceratti da Rocha <andre.ceratti.rocha@gmail.com>
2020-08-18 16:18:03 -0300

    +IDK Bro, many things

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  avaible: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    console.log(appointments);
+    return [{ day: 1, avaible: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
